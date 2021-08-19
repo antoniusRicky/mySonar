c6d55703dcfdb6f060c5fae4b52c2d3bec5e8c37
antoniusRicky <69069663+antoniusRicky@users.noreply.github.com>
2021-08-19 13:24:56 +0700

    Update Main.java

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -1,12 +1,10 @@
 package com.sample;
 
 public class Main {
-	public static final String CONSTANT1 = "abc";
-	public static final String CONSTANT2 = CONSTANT1 + "def";
+	public String CONSTANT1 = "abc";
+	public String CONSTANT2 = CONSTANT1 + "def";
 	
 	public static void main(String[] args) {
 		System.out.println(CONSTANT2);
-		while(true) {
-		}
 	}
 }
