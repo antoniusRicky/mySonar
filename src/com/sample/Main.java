dd8172dbac45e29907470da5de533e11725cce29
antoniusRicky <69069663+antoniusRicky@users.noreply.github.com>
2021-08-19 15:07:04 +0700

    Update Main.java

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -3,8 +3,15 @@ package com.sample;
 public class Main {
 	public String CONSTANT1 = "ddsd";
 	public String CONSTANT2 = CONSTANT1 + "def";
+	int a = 0;
+	int b = 1;
 	
 	public static void main(String[] args) {
-		System.out.println(CONSTANT2);
+		if (a == b) {
+			System.out.println(CONSTANT2);
+		}
+		else {
+			System.out.println("00000000");
+		}
 	}
 }
