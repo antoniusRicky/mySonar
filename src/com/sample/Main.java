12fcfb53b7777a83fd3762efbaa3c89eced11039
antoniusRicky <69069663+antoniusRicky@users.noreply.github.com>
2021-08-19 13:26:44 +0700

    Update Main.java

--- src/com/sample/Main.java
+++ src/com/sample/Main.java
@@ -1,7 +1,7 @@
 package com.sample;
 
 public class Main {
-	public String CONSTANT1 = "abc";
+	public String CONSTANT1 = "ddd";
 	public String CONSTANT2 = CONSTANT1 + "def";
 	
 	public static void main(String[] args) {
